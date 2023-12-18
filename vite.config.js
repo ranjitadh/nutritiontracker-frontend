bd94e36599f118e2f05f3b5a12bb2db16a0009a8
Amrit Pandey <kabirjungpandey@gmail.com>
2023-12-18 10:57:42 +0545

    build fail debug

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import 'vite/modulepreload-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     manifest: true,
-    rollupOptions: {
-      input: 'path/to/main.js',
-    }
-  },
+    },
   plugins: [react()],
 })
